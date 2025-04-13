83fc3d74d1d1a7d864acd5edf2c9fd4c4709ddb1
Cong Le <cle155@csu.fullerton.edu>
2025-04-13 05:28:20 -0700

    setting up

---
+++ Pixel_Palette_Source_Code/Pixel_Palette/Pixel_PaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_PaletteApp.swift
+//  Pixel_Palette
+//
+//  Created by Cong Le on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_PaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pixel_Palette_Source_Code/Pixel_PaletteUITests/Pixel_PaletteUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Pixel_PaletteUITestsLaunchTests.swift
+//  Pixel_PaletteUITests
+//
+//  Created by Cong Le on 4/13/25.
+//
+
+import XCTest
+
+final class Pixel_PaletteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
